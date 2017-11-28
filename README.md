e4c21e737edc6629a5d73c8333b5d9a71e185d3c
hartmanningvarsson <svuntutussa@gmail.com>
2017-11-28 16:43:42 +0000

    Update - Harti 28.11.17

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # VLN1_TeamSupergroup
 RU_T-113_VLN_1-Group:6
+
+Háskólinn í Reykjavík, Haust 2017
+
+Elfar Már Viggósson
+Hartmann Ingvarsson
+Filippus Darri Björgvinsson
+
+Leiðbeinandi
+Egill Anton Hlöðversson
+
+--------------------------
+	Status:
+--------------------------
+Verkefni 1 = Done
+Main = ?
--- Verkefni1/Verkefni1_A(Part1)/main.cpp
+++ Verkefni1/Verkefni1_A(Part1)/main.cpp
@@ -4,35 +4,34 @@
 
 //fafafafaf
 
-const int Max_Lines = 10;
 
 using namespace std;
 
 int main()
 {
-    string read_10_lines;
+    string read_line;
     char choice;
 
     ifstream fin;
     fin.open("textFile.txt");
 
     do{
-        if (fin.eof()){                                     /// If setning sem tjekkar hvort vid erum kominn i botn a skjalinu, ef svo tha brjota loopu.
+        if (fin.eof()){
             cout << "End of document";
             cout << endl;
             break;
         }
 
-        if(fin.is_open()){                                         /// "Opna skjalid og itra med for-loopu til thess ad fa 10 linur
-            for(int i = 0; i < Max_Lines; i++){
-                cout << "Line nr: " << i + 1 << " ";        /// Til thess ad sja hvada linu vid erum ad prenta
-                getline(fin, read_10_lines);
-                cout << read_10_lines << " " << endl;
+        if(fin.is_open()){
+            for(int i = 0; i < sizeof(read_line); i++){
+                getline(fin, read_line);
+                cout << read_line << " " << endl;
 
                 }
         }
 
-            cout << "Do you want to continue y/n? ";                /// Y og hvada char sem er, fyrir utan "n" heldur afram thar til skjalid endar.
+            cout << "Do you want to continue y/n? ";
+
 
         do{
             cin >> choice;
