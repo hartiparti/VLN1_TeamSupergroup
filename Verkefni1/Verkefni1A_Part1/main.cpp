19bab0df6bde079bc6eeeb576016737642267a32
hartmanningvarsson <svuntutussa@gmail.com>
2017-11-28 22:32:01 +0000

    Test2

--- Verkefni1/Verkefni1A_Part1/main.cpp
+++ Verkefni1/Verkefni1A_Part1/main.cpp
@@ -2,70 +2,44 @@
 #include <fstream>
 #include <string>
 
+
 const int size_of_output = 10;
 using namespace std;
-
-int check(char& choice){
-    if((choice == 'n') || (choice == 'N')){
-    cout << "Exiting program";
-        return 1;
-    }
-    else if ((choice == 'y') || (choice == 'y')){
-        return 2;
-    }
-    else {
-        return 3;
-    }
-
-}
-
-void print(ifstream& fin, string& read_line, int& counter){
-
-            for(int i = 0; i < size_of_output; i++){
-            getline(fin, read_line);
-            counter++;
-            cout << "Line nr: " << counter << " :";
-            cout << read_line << " " << endl;
-    }
-
-}
 
 int main()
 {
     string read_line;
     char choice;
     int counter = 0;
-    int start = 0;
 
     ifstream fin;
     fin.open("textFile.txt");
-    while(start == 0){
-        if(fin.is_open()){
-            print(fin, read_line, counter);
-            cin >> choice;
-            for(int i = 0; i < size_of_output; i++){
-
-            if (fin.eof()){
-                break;
-            }
-            if(check(choice) == 1){
+    do{
+        if((choice == 'n') || (choice == 'N')){
+                cout << "Exiting program";
                 return 0;
             }
-            else if (check(choice) == 2){
-                print(fin, read_line, counter);
-            }
-            else if (check(choice)== 3){
+        if(fin.is_open()){
+        for(int i = 0; i < size_of_output; i++){
+            getline(fin, read_line);
+                if (fin.eof()){
                 break;
+                }
+                counter++;
+            cout << "Line nr: " << counter << " :";
+            cout << read_line << " " << endl;
             }
+            cout << "--------------------------------" << endl;
+            cout << "Do you want to continue y/n? ";
+            cout << endl;
+            cin >> choice;
+             if (fin.eof()){
+                break;
+                }
+            }
+        }while((choice != 'y') || (choice != 'n'));
 
-
-
-        }
-
-
-    }
-    }
-    cout << "End of file, exiting";
+            cout << "End of file, exiting";
     fin.close();
     return 0;
 }
