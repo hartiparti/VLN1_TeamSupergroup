70ce5ef255233d4c30cc9d76ebba10373e96c75a
Elfarv <elfarv@gmail.com>
2017-12-01 14:27:45 +0000

    baker class

---
+++ SuperGroup_PizzaCo/Baker.cpp
@@ -0,0 +1,3 @@
+#include "Main_interface.h"
+#include <iostream>
+#include <stdlib.h>
---
+++ SuperGroup_PizzaCo/Baker.h
@@ -0,0 +1,18 @@
+#ifndef BAKER_H
+#define BAKER_H
+
+
+class Baker{
+
+private:
+
+
+
+public:
+
+}
+
+
+
+
+#endif
