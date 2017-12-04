07427ab9e8a6fdaa0fc7a55313e4db3d28719c33
hartmanningvarsson <svuntutussa@gmail.com>
2017-12-04 09:45:51 +0000

    Setti inn Verkefni2

---
+++ SuperGroup_PizzaCo/Order_UI.cpp
@@ -0,0 +1,11 @@
+#include "Order_UI.h"
+
+Order_UI::Order_UI()
+{
+    //ctor
+}
+
+Order_UI::~Order_UI()
+{
+    //dtor
+}
---
+++ SuperGroup_PizzaCo/Order_UI.h
@@ -0,0 +1,16 @@
+#ifndef ORDER_UI_H
+#define ORDER_UI_H
+
+
+class Order_UI
+{
+    public:
+        Order_UI();
+        virtual ~Order_UI();
+
+    protected:
+
+    private:
+};
+
+#endif // ORDER_UI_H
